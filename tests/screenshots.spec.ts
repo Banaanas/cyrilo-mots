e04a5df17b5cbc2c8a82d3e151d20b023598575c
Cyril <cyrildeveloppeur@gmail.com>
2022-08-28 23:19:28 +0200

    feat: new Playwright Snapshots

--- tests/screenshots.spec.ts
+++ tests/screenshots.spec.ts
@@ -3,12 +3,15 @@ import { expect, test } from "@playwright/test";
 /* ** Test if screenshots are identical ** */
 
 // To update screenshots: npx playwright test --update-snapshots
+// https://playwright.dev/docs/test-snapshots
 test("LoginPage Screenshots are identical", async ({ page }) => {
   await page.goto("/login");
 
   await page.waitForTimeout(5000);
 
+  // Expect the Full Page Screenshot to be identical as the previous one
   await expect(page).toHaveScreenshot({
+    fullPage: true,
     maxDiffPixels: 100,
     animations: "disabled", // Disable animations
   });
