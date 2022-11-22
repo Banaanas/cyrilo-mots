02a069ce76a7305fe2a9979488d36456b43ce3e4
Cyril <cyrildeveloppeur@gmail.com>
2022-11-22 21:01:16 +0100

    refactor(api): delete unused api file

--- pages/api/auth/[...supabase].ts
+++
@@ -1,5 +0,0 @@
-import { handleAuth } from "@supabase/auth-helpers-nextjs";
-
-import { navLinks } from "../../../data/navlinks";
-
-export default handleAuth({ logout: { returnTo: navLinks.login.href } });
