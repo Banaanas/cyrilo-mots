6191465fccbb9a44c9880bb3cb20ca0189f81e2c
Cyril <cyrildeveloppeur@gmail.com>
2022-11-22 21:02:50 +0100

    feat(_app): set up supabase provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,40 @@
 import "../styles/normalize.css"; // Next.js authorizes Normalize.css (Global CSS) to be imported ONLY from _app.js
 
-import { supabaseClient } from "@supabase/auth-helpers-nextjs";
-import { UserProvider } from "@supabase/auth-helpers-react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
-import React from "react";
+import React, { useState } from "react";
 
 import Layout from "../Components/Layout/Layout";
 import DefaultSEO from "../SEO/next-seo.config";
 import getReactAxe from "../utils/get-react-axe";
 
 // Add React Axe in Development Mode
-
 if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
   // eslint-disable-next-line no-void
   void getReactAxe();
 }
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) => {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
     <>
       <DefaultSeo {...DefaultSEO} />
-      <UserProvider supabaseClient={supabaseClient}>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </UserProvider>
+      </SessionContextProvider>
     </>
   );
 };
